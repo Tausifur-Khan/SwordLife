d8b6aeb4edc26883303013d68582ad6a3290bf4d
Tausifur <ultimatepow24@gmail.com>
2019-02-20 15:05:29 +1100

    Jump Fix

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -11,6 +11,7 @@ namespace knight
         #region Movement Variables
         public float mvSpeed = 2;
         public float jumpForce = 50;
+        public float jumpDir;
 
         #endregion
         //2d rigidbody component
@@ -19,6 +20,9 @@ namespace knight
         private Animator anim;
         //sprite renderer
         private SpriteRenderer sprite;
+
+
+
         // Start is called before the first frame update
         void Start()
         {
@@ -37,23 +41,27 @@ namespace knight
         }
         private void FixedUpdate()
         {
-            
+
         }
 
         void Move()
         {
+            // access to input axis within horizontal section
             float inputx = Input.GetAxis("Horizontal");
 
-            Vector2 mvDir = new Vector2(inputx, 0);
-            rb2d.velocity = mvDir * mvSpeed;
+            //rigidbody velocity is equal to new vector2 space
+            //new vector space with x value , y normal0
+            rb2d.velocity = new Vector2(inputx * mvSpeed, rb2d.velocity.y);
+            
 
             //if this key is pressed then
             if (Input.GetKeyDown(KeyCode.W))
-            {        
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
                 //add force going up within a 2d vector 
-                rb2d.AddForce(Vector2.up * jumpForce);
+               // rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
             }
-            
+
 
             #region Sprite Flip Condition
             // sprite flip
@@ -61,7 +69,7 @@ namespace knight
             {
                 sprite.flipX = true;
             }
-            if(inputx >= 0.1)
+            if (inputx >= 0.1)
             {
                 sprite.flipX = false;
             }
