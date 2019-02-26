7fb34852d29c7cd5b4f065ed131ac0d7d7ba0e18
Tausifur <ultimatepow24@gmail.com>
2019-02-26 15:00:11 +1100

    Dash timer

--- Assets/Scripts/CharacterScripts/CharacterController.cs
+++ Assets/Scripts/CharacterScripts/CharacterController.cs
@@ -7,6 +7,8 @@ namespace Knight
 {
     public class CharacterController : MonoBehaviour
     {
+        #region Variables
+
         #region Movement Variables X & Y
         [Header("Movement Variable x & y")]
         //float speed 
@@ -18,6 +20,7 @@ namespace Knight
         //double jump bool condition
         public bool doubleJump;
         [Space(2)]
+        #endregion
 
         #region Dash Variables
         [Header("Dash Variables")]
@@ -26,10 +29,15 @@ namespace Knight
         //bool condition for dash
         public bool isDash = true;
         //Dash timers min & max
-        public float dashTime = 1;
-        private float timer = 0;
+        public float dashMaxTime;
+        public float dashtimer;
+        [Space(2)]
         #endregion
 
+        #region Keycodes
+        [Header("Keycodes")]
+        public KeyCode left = KeyCode.A;
+        public KeyCode right = KeyCode.D;
         #endregion
 
         #region Private Variable Components
@@ -41,6 +49,7 @@ namespace Knight
         private SpriteRenderer sprite;
         #endregion
 
+        #endregion
 
         // Start is called before the first frame update
         void Start()
@@ -54,17 +63,23 @@ namespace Knight
             isDash = true;
             #endregion
 
+            #region Component references
             //Refer to rigid component
             rb2d = GetComponent<Rigidbody2D>();
             //refer to animation component
             anim = GetComponent<Animator>();
             //refer to sprite renderer component
             sprite = GetComponent<SpriteRenderer>();
+            #endregion
+
+            dashtimer = dashMaxTime;
+
         }
 
         void Update()
         {
             Jump();
+            Dash();
 
         }
 
@@ -76,74 +91,107 @@ namespace Knight
         //Method: Movement in X direction with velocity
         void Move()
         {
+            //Get Axis
+            #region Move with getaxis horizontal
             // access to input axis within horizontal section
-            float inputx = Input.GetAxis("Horizontal");
+            //float inputx = Input.GetAxis("Horizontal");
 
             //rigidbody velocity is equal to new vector2 space
             //new vector space with x value , y normal0
-            rb2d.velocity = new Vector2(inputx * mvSpeed, rb2d.velocity.y);
+            //rb2d.velocity = new Vector2(inputx * mvSpeed, rb2d.velocity.y);
+            #endregion
 
-            //if not movement in left or right then...
-            if(inputx == 0)
+            //Get Key
+            #region Alternate movement
+            if (Input.GetKey(left))
             {
-                //set velocity movement in x to 0
-                rb2d.velocity = new Vector2(0, rb2d.velocity.y);
-            }
-
-            //if dash bool condition true then...
-            if (isDash)
-            {
-                //Dash Movement
-                //if input key 'space' then...
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    isDash = false;
-                    
-                    // if horizontal direction is right then...
-                    if (Input.GetAxis("Horizontal") >= 0.1)
-                    {
-                        // allow force for dash in right dir
-                        rb2d.AddForce(Vector2.right * dashForce, ForceMode2D.Impulse);
-                    }
-                    //if horizontal direction is left then...
-                    if (Input.GetAxis("Horizontal") <= -0.1)
-                    {
-                        // allow force for dash in left dir
-                        rb2d.AddForce(Vector2.left * dashForce, ForceMode2D.Impulse);
-                    }
-                }
-            }
-
-            
-            #region Sprite Flip Condition
-            // sprite flip left
-            if (inputx <= -0.1)
-            {
+                rb2d.velocity = new Vector2(-mvSpeed, rb2d.velocity.y);
                 sprite.flipX = true;
+                anim.SetBool("isMoving", true);
             }
-
-            //sprite flip right
-            if (inputx >= 0.1)
+            else if (Input.GetKey(right))
             {
+                rb2d.velocity = new Vector2(mvSpeed, rb2d.velocity.y);
                 sprite.flipX = false;
+                anim.SetBool("isMoving", true);
             }
+            else
+            {
+                rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+                anim.SetBool("isMoving", false);
+            }
+            #endregion
+
+            #region Sprite Flip Condition
+            //// sprite flip left
+            //if (inputx <= -0.1)
+            //{
+            //    sprite.flipX = true;
+            //}
+
+            ////sprite flip right
+            //if (inputx >= 0.1)
+            //{
+            //    sprite.flipX = false;
+            //}
             #endregion
 
             #region Animation Condition
             //animation set
-            if (inputx >= 0.1 || inputx <= -0.1)
-            {
-                anim.SetBool("isMoving", true);
+            //if (inputx >= 0.1 || inputx <= -0.1)
+            //{
+            //    anim.SetBool("isMoving", true);
 
-            }
-            else
+            //}
+            //else
+            //{
+            //    anim.SetBool("isMoving", false);
+            //}
+            #endregion
+        }
+
+
+        void Dash()
+        {
+            #region Dash Movement
+            //if dash bool condition true & timer is not 0 then...
+            if (isDash && dashtimer > 0)
             {
-                anim.SetBool("isMoving", false);
+
+                //if input key 'space' then...
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    // if horizontal direction is right then...
+                    if (Input.GetKey(left))
+                    {
+                        // allow force for dash in right dir
+                        rb2d.AddForce(Vector2.left * dashForce, ForceMode2D.Impulse);
+                        //start timer
+                        Timer();
+
+                    }
+                    //if horizontal direction is left then...
+                    if (Input.GetKey(right))
+                    {
+                        // allow force for dash in left dir
+                        rb2d.AddForce(Vector2.right * dashForce, ForceMode2D.Impulse);
+                        //start timer
+                        Timer();
+                    }
+                }
+                //otherwise
+                else
+                {
+                    
+                }
             }
+
+
             #endregion
         }
 
         //Method: Movement in Y direction with velocity & Animation
+        //Jump Movement
         void Jump()
         {
             //if this key is pressed & player is grounded then...
@@ -186,5 +234,12 @@ namespace Knight
                 anim.SetBool("isJumping", false);
             }
         }
+
+        void Timer()
+        {
+            //Start timer 
+            dashtimer -= Time.deltaTime;
+        }
     }
+
 }
