c316419463cd40167c8e2d5d645b61becc3c4b23
Tausifur <ultimatepow24@gmail.com>
2019-04-19 15:05:22 +1000

    LevelSetup & Cooldown ui

---
+++ Assets/Scripts/UI(Game)/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text dashTimeTxt;
+    public Text RangeTimeTxt;
+
+    public Knight.CharacterController charC;
+    public Knight.RangeTimer rangeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
