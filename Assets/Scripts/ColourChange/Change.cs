32dc9a87667443ca9943babe4344fe8de8f8603e
Tausifur <ultimatepow24@gmail.com>
2019-05-29 14:27:14 +1000

    Level 2

--- Assets/Scripts/ColourChange/Change.cs
+++ Assets/Scripts/ColourChange/Change.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,58 +9,35 @@ namespace Knight
     public class Change : MonoBehaviour
     {
         public SpriteRenderer sRender;
-        public float curHp;
-        public float maxHp;
-
-        public Slider enemyHp;
-
-        public GameObject enemySlider;
-
+       
        public Attack playerAttack;
 
         // Start is called before the first frame update
         void Start()
         {
             sRender = GetComponent<SpriteRenderer>();
-            curHp = Mathf.Abs(maxHp);
-            enemySlider.SetActive(false);
+          
         }
-
-
-
-        void LateUpdate()
-        {
-            enemyHp.value = curHp;
-            EnemyDeath();
-        }
-
+        
         private void OnTriggerEnter2D(Collider2D col)
         {
             if (col.gameObject.name == "AttackZone")
             {
                 Debug.Log("Getting Attack: ");
-                enemySlider.SetActive(true);
+               
                 ChangeColor();
               
-                curHp -= playerAttack.playerGroundDmg;
+                
             }
         }
-
+    
         void ChangeColor()
         {
             //change material color of gameobject
             sRender.material.color = Random.ColorHSV();
         }
 
-        void EnemyDeath()
-        {
-            if (curHp <= 0)
-            {
-                // Destroy(gameObject);
-                //enemySlider.SetActive(false);
-                curHp = maxHp;
-            }
-        }
+       
 
     }
 }
