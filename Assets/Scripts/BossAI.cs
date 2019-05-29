96a0c481a3fff31c76ef8931d4f9176c4e9f2ff1
H A <hugoa3520@gmail.com>
2019-05-29 14:31:08 +1000

    tiny update

---
+++ Assets/Scripts/BossAI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAI : Enemy
+{
+    public bool isAttacking;
+    public int attackMode;
+    private float attackCounter;
+    public float attackTimer = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(attackCounter <= 0)
+        {
+            switch (attackMode)
+            {
+                case 0:
+                    attackCounter = 1.2f;
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+            }
+            attackCounter += attackTimer;
+        }
+        attackCounter -= Time.deltaTime;
+    }
+}
