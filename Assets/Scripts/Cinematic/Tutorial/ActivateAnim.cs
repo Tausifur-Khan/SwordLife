88fa6bf0c841334f91dae2b959619f225cb583fe
TausK <36681180+TausK@users.noreply.github.com>
2019-06-17 15:14:10 +1000

    Fix

---
+++ Assets/Scripts/Cinematic/Tutorial/ActivateAnim.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace AnimationSet
+{
+    public class ActivateAnim : MonoBehaviour
+    {
+        [SerializeField]
+        Knight.PlayerController player;
+
+        [SerializeField]
+        GameObject wizardAnim;
+
+
+        private void Start()
+        {
+            wizardAnim.SetActive(false);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                //activate animation with bool
+                
+                wizardAnim.SetActive(true);
+                //disable player movement with bool
+                player.restrictMove = true;
+
+            }
+        }
+
+       
+    }
+}
